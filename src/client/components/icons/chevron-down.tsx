87ef46cc9b5c42b3cc016c7fd9f1306bb766b4e0
Charmaine Wang <charmainewang424@hotmail.com>
2025-06-02 10:12:23 +0200

    chevron for dropdown

---
+++ src/client/components/icons/chevron-down.tsx
@@ -0,0 +1,18 @@
+export const ChevronDownIcon = ({ size = 16 }: { size?: number }) => {
+  return (
+    <svg
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 16 16"
+    >
+      <path
+        d="M15.5 4.874 8.353 12.02a.5.5 0 0 1-.706 0L.5 4.874"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
