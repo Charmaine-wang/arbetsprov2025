1d7f9c72b729ee3a88098b60eb14e7c24b130ca6
Charmaine Wang <charmainewang424@hotmail.com>
2025-06-03 14:34:27 +0200

    check icon

---
+++ src/client/components/icons/check.tsx
@@ -0,0 +1,19 @@
+export const CheckIcon = ({ size = 16 }: { size?: number }) => {
+  return (
+    <svg
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 30"
+    >
+      <path
+        d="m5 13.48 4.347 7.67c.278.514.74.83 1.239.85.5.018.975-.264 1.277-.757L19 8"
+        stroke="#008000"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
